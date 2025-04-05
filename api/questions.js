9d464aa3592aa36e67cdd8125863b72974fd414f
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-04-06 00:05:15 +0200

    json file edir

--- api/questions.js
+++ api/questions.js
@@ -4,13 +4,19 @@ import path from 'path';
 
 export default function handler(req, res) {
   const server = jsonServer.create();
+  const filePath = path.join(__dirname, '../../data/questions.json');
   
-  // استخدم المسار المطلق
-  const router = jsonServer.router(path.join(__dirname, '../../data/questions.json'));  
+  const router = jsonServer.router(filePath);
   const middlewares = jsonServer.defaults();
 
   server.use(middlewares);
-  server.use(router);
-
-  server.handle(req, res);  // التعامل مع الطلبات بشكل صحيح
+  server.use((req, res, next) => {
+    res.on('finish', () => {
+      console.log('Response status:', res.statusCode);
+      console.log('Response body:', res.body);
+    });
+    next();
+  });
+  
+  server.handle(req, res);
 }
