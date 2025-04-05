abd4813c023d79dc8f4703479eba95afc1bd8c65
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-04-06 00:27:14 +0200

    edit api path

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ function App() {
   const maxPoints = questions?.reduce((prev, curr) => prev + curr.points, 0);
   useEffect(function () {
     // fetch("http://localhost:9000/questions")
-    fetch("/api/questions")
+    fetch("https://react-quiz-lemon-one.vercel.app/api/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecived", payload: data }))
       .catch((err) => console.error({ type: "dataFailed", payload: err }));
