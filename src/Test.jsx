b7bb417fe858dc5f2cc79f12a4c302dc37c0c195
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-04-06 21:31:46 +0200

    disabledNextButton

--- src/Test.jsx
+++ src/Test.jsx
@@ -28,21 +28,33 @@ export default function Test({
         <Optoins questions={questions} dispatch={dispatch} answer={answer} />
       </div>
 
+      {answer ? console.log(answer) : null}
       <div className="flex items-center justify-end">
-        <button
-          className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-          onClick={() => {
-            if (index < 14) {
-              dispatch({ type: "next", payload: index });
-            } else {
-              dispatch({ type: "finish" });
-            }
-          }}
-        >
-          <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
-            Next
-          </span>
-        </button>
+        {answer !== null ? (
+          <button
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+            onClick={() => {
+              if (index < 14) {
+                dispatch({ type: "next", payload: index });
+              } else {
+                dispatch({ type: "finish" });
+              }
+            }}
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+              Next
+            </span>
+          </button>
+        ) : (
+          <button
+            disabled
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+              Next
+            </span>
+          </button>
+        )}
       </div>
     </div>
   );
